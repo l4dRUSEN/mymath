6038db464d6e2421e3b62006c38cefeb04d43f02
l4dRUSEN <97851931+l4dRUSEN@users.noreply.github.com>
2022-12-20 01:00:28 +0500

    Update addition.go

--- addition.go
+++ addition.go
@@ -1,5 +1,20 @@
+// File: mymath/go.mod
+module github.com/username/mymath
+
+go 1.16
+
+// File: mymath/addition.go
 package mymath
 
 func Add(a, b int) int {
     return a + b
 }
+
+// File: main/go.mod
+module main
+
+go 1.16
+
+require (
+    github.com/username/mymath latest
+)
